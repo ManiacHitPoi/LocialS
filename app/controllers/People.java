c9ee0d6f74f581a2601bb5042b7c130ccb6c96b0
Akira Nakazawa <nakazawa.akira@gmail.com>
2011-08-27 03:45:28 +0900

    first commit for search(). More elegant solution is needed.

--- app/controllers/People.java
+++ app/controllers/People.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import models.Content;
 import models.Person;
 import models.Photo;
+
 import play.mvc.Controller;
 
 public class People extends Controller {
@@ -24,10 +25,41 @@ public class People extends Controller {
     }
     
     // ToDo
-    public static void serach(String name, Integer age, Boolean sex, Integer area) {
-        Person person = Person.findById(new Long(1));
-        List<Content> list = person.contents;        
-    }    
+    public static void search(String name, Integer age, Boolean sex, Integer area) {
+ 
+        // TODO
+        //if (validation.hasErrors()) {
+        //}
+        
+        String query = "select p from Person p where ";
+        
+        query += "p.name like ?";
+                
+        if (age != null) {
+            query += " and p.ageId is " + age;
+        }
+
+        if (sex != null) {
+            query += " and p.sex = " + sex;            
+        }
+
+        if (area != null) {
+            query += " and p.areaId is " + area;
+        }
+        
+        List<Person> people = Person.find(query, name + "%").fetch();
+        //List<Person> people = Person.find("select p from Person p where p.name like ?", name+"%").fetch();
+        //List<Person> people = Person.find("select p from Person p where p.name like ? and p.sex = false", name+"%").fetch();
+        //List<Person> people = Person.find("select p from Person p where p.name like ? and p.sex = ? and p.ageId = ? and areaId = ?", name+"%", sex, age, area).fetch();
+        renderArgs.put("people", people);
+        render();
+
+        //Query query = JPA.em().createQuery("select c from Comment c");
+        //List<Comment> comments = query.getResultList();
+        //renderArgs.put("comments", comments);
+        //render();
+        
+    }
 
     public static void login(String name) {
         List<Person> people = Person.find("byName", name).fetch();
@@ -40,7 +72,6 @@ public class People extends Controller {
         List<Person> people = null;
         people = Person.find("byId", id).fetch();
         renderArgs.put("people", people);
-        //response.encoding = "ISO-8859-1";
         render();
     }
     
