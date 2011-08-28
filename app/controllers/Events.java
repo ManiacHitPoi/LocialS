849afd81582b816479b24f2fa41577227da93f5d
taiz <taiz7715@gmail.com>
2011-08-28 15:53:36 +0900

    date format

--- app/controllers/Events.java
+++ app/controllers/Events.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.io.FileInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import play.Logger;
@@ -18,28 +19,30 @@ import play.mvc.Controller;
 
 public class Events extends Contents {
 
-    public static void create(Long id, String title, Date eventDate,
+    public static void create(Long id, String title, String eventDate,
             File image, Integer area) throws FileNotFoundException {
         Person person = Person.findById(id);
         Blob photo = new Blob();
 
         photo.set(new FileInputStream(image.getAbsolutePath()),
                 MimeTypes.getContentType(image.getName()));
-        Event content = new Event(person, title, eventDate, photo, area);
+        Event content = new Event(person, title, string2date(eventDate), photo, area);
         content.save();
         render(content);
     }
-    
-    public static void list() {
+
+    public static void list(Long id) {
         Date today = new Date();
         List<Event> eventList = null;
-        Long userId = params.get("id", Long.class);
+        //Long userId = params.get("id", Long.class);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        if (userId == -1) {
+        System.err.println(formatter.format(today));
+        if (id == -1) {
             eventList = Event.find("eventdate >= ? order by eventDate desc", formatter.format(today)).fetch();
-            
+            //eventList = Event.findAll();
+
         } else {
-            eventList = Event.find("owner_id = ? and eventdate >= ? order by eventDate desc", userId,formatter.format(today)).fetch();
+            eventList = Event.find("owner_id = ? and eventdate >= ? order by eventDate desc", id,formatter.format(today)).fetch();
         }
 
         //List<Event> eventList = Event.find("id > ?", new Long(45)).fetch();
@@ -47,5 +50,14 @@ public class Events extends Contents {
         //List<Event> eventList = Event.findAll();
         renderArgs.put("eventList", eventList);
         render();
+    }
+
+    private static Date string2date(String strdate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        try {
+            return sdf.parse(strdate);
+        } catch (ParseException e) {
+            return null;
+        }
     }
 }
